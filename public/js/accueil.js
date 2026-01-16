2783fe85f70d60379e38504768aaf5fde3765405
kuroi-hime <kuroihime095@gmail.com>
2026-01-16 20:56:29 +0100

    commun files

---
+++ public/js/accueil.js
@@ -0,0 +1,12 @@
+const searchCar = document.getElementById('search_vehicules');
+const resultsSearchVehiculesContainer = document.getElementById('search_vehicules_results');
+
+// searchCar.addEventListener('input', ()=>{
+//     let search = this.value;
+//     fetch() //Comment récupperer les données
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById('resultGrid').innerHTML = data;
+//         })
+//         .catch(error => console.error('Erreur:', error));
+// });
